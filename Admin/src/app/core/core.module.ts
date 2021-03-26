c3e58d3d2a526f92f622a2fb07c8598932b4b538
Dreed657 <ninjakiviWTF@gmail.com>
2021-03-26 18:24:59 +0200

    Integrated admin panel theme

--- Admin/src/app/core/core.module.ts
+++ Admin/src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, SidebarComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HeaderComponent, SidebarComponent]
 })
 export class CoreModule { }
