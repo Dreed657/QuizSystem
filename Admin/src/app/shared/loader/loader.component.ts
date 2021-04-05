9bd3c44adcc9145e7d5dd92a505e5ea0f6e765fc
Dreed657 <ninjakiviWTF@gmail.com>
2021-04-05 23:34:45 +0300

    Add loading spinner

--- Admin/src/app/shared/loader/loader.component.ts
+++ Admin/src/app/shared/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
+
+  @Input() isLoading = false;
 
   constructor() { }
 
