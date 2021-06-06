8e41bfab1c9e45ac761028001576df98b191357a
Dreed657 <ninjakiviwft@gmail.com>
2021-06-06 19:21:41 +0300

    Add custom mapping for ExamsAttempted

--- Server/Server/Models/User/UserViewModel.cs
+++ Server/Server/Models/User/UserViewModel.cs
@@ -1,9 +1,10 @@
-﻿using Server.Data.Models;
+﻿using AutoMapper;
+using Server.Data.Models;
 using Server.Infrastructure.Mappings.Contracts;
 
 namespace Server.Models.User
 {
-    public class UserViewModel : IMapFrom<ApplicationUser>
+    public class UserViewModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
     {
         public string Id { get; set; }
 
@@ -14,5 +15,14 @@ namespace Server.Models.User
         public string LastName { get; set; }
 
         public int ExamsAttempted { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<ApplicationUser, UserViewModel>()
+                .ForMember(
+                    x => x.ExamsAttempted,
+                    opt => opt.MapFrom(y => y.Exams.Count)
+                );
+        }
     }
 }
