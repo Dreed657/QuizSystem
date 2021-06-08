e3731f9438d8b3332afb90abf54f1682ca20ef3c
Dreed657 <ninjakiviWTF@gmail.com>
2021-06-08 16:04:28 +0300

    Update profile view model and controller

--- Server/Server/Controllers/ProfileController.cs
+++ Server/Server/Controllers/ProfileController.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Server.Services.Common;
 using Server.Services.Profile;
 
 namespace Server.Controllers
@@ -7,16 +9,37 @@ namespace Server.Controllers
     public class ProfileController : ApiController
     {
         private readonly IProfileService _profileService;
+        private readonly ICurrentUserService _currentUser;
 
-        public ProfileController(IProfileService profileService)
+        public ProfileController(IProfileService profileService, ICurrentUserService currentUser)
         {
             _profileService = profileService;
+            _currentUser = currentUser;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserProfile()
+        {
+            var userId = this._currentUser.GetId();
+            var result = await this._profileService.GetProfile(userId);
+
+            if (result == null)
+            {
+                return this.BadRequest(result);
+            }
+
+            return this.Ok(result);
         }
 
 
-        [HttpGet]
-        public async Task<IActionResult> GetProfile(string id)
+        [HttpGet(nameof(GetProfileByUserId))]
+        public async Task<IActionResult> GetProfileByUserId([FromQuery] string id)
         {
+            if (string.IsNullOrEmpty(id))
+            {
+                return this.BadRequest("Id query parameter is required!");
+            }
+
             var result = await this._profileService.GetProfile(id);
 
             if (result == null)
--- Server/Server/Models/Profile/ProfileViewModel.cs
+++ Server/Server/Models/Profile/ProfileViewModel.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
 using Server.Data.Models;
 using Server.Infrastructure.Mappings.Contracts;
 using Server.Models.ExamAttempt;
 
 namespace Server.Models.Profile
 {
-    public class ProfileViewModel : IMapFrom<ApplicationUser>
+    public class ProfileViewModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
     {
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
+        public double AvgGPA { get; set; }
+
         public ICollection<ExamAttemptViewModel> Exams { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<ApplicationUser, ProfileViewModel>()
+                .ForMember(
+                    x => x.AvgGPA,
+                    opt => opt.MapFrom(
+                        y => Math.Round(y.Exams.Average(a => a.Score), 2)
+                    )
+                );
+        }
     }
 }
